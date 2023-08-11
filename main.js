9127d20ea4810ab825c0fcbf19f7f8460220bb85
Koltronn <74987336+Koltronn@users.noreply.github.com>
2023-08-11 00:12:23 -0500

    added grid size dropdown

--- main.js
+++ main.js
@@ -1,27 +1,49 @@
 let bigContainer = document.getElementById('bigContainer')
 
-bigContainer.setAttribute('style', 'display: flex;flex-wrap: wrap;align-items: center;justify-content: center;padding: 0;border: .5px;border-color: black;border-style: solid;max-width: 640px;margin: 0 auto')
+bigContainer.setAttribute('style', 'display: flex;flex-wrap: wrap;align-items: center;justify-content: center;padding: 0;border: .5px;border-color: black;border-style: solid;max-width: 640px;max-height:640px;margin: 0 auto')
 
-for (x = 0; x < 256; x++){
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    bigContainer.appendChild(cell);
-    cell.setAttribute('style', 'border: 0px;margin: 0;min-width: 40px;min-height: 40px')
-    
+function gridSize(num, width, height){
+    for (x = 0; x < num; x++){
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        bigContainer.appendChild(cell);
+        cell.setAttribute('style', `border: 0px;border-color:black;border-style:solid;margin: 0;min-width: ${width};min-height: ${height}`)
+        
+    }
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(function(cell){
+        cell.addEventListener('mouseover', function(e){
+            cell.style.background="teal";
+        })
+    })
+    let resetBtn = document.querySelector('button')
+    resetBtn.setAttribute('style', 'color: white; background-color: black; font-size:30px; display: flex; margin: 0 auto; margin-top: 40px')
+    resetBtn.addEventListener('click', function(){
+        cells.forEach(function(cell){
+            cell.style.background="#FFF"
+        })
+    })
 }
 
-let cells = document.querySelectorAll('.cell');
-cells.forEach(function(cell){
-    cell.addEventListener('mouseover', function(e){
-        cell.style.background="#000";
-    })
+gridSize(64, '80px', '80px')
+
+let sizeOptions = document.getElementById('grid-sizes')
+sizeOptions.addEventListener('click', function(){
+    if(this.value === '8x8'){
+        bigContainer.innerHTML = '';
+        gridSize(64, '80px', '80px')
+    } else if(this.value === '16x16'){
+        bigContainer.innerHTML = '';
+        gridSize(256, '40px', '40px')
+    } else if(this.value === '32x32'){
+        bigContainer.innerHTML = '';
+        gridSize(1024, '20px', '20px')
+    } else if(this.value === '64x64'){
+        bigContainer.innerHTML = '';
+        gridSize(4096, '10px', '10px')
+    }  return;
 })
 
 
-let resetBtn = document.querySelector('button')
-resetBtn.setAttribute('style', 'color: white; background-color: black; font-size:50px; display: flex; margin: 0 auto; margin-top: 50px')
-resetBtn.addEventListener('click', function(){
-    cells.forEach(function(cell){
-        cell.style.background="#FFF"
-    })
-})
+let titleContainer = document.querySelector('h1')
+titleContainer.setAttribute('style', 'text-align:center;font-size:60px')
